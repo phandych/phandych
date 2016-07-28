1222f339adbe1fd4e67671c36f8971bad3da2a48
phandych <phandy.chandra@gmail.com>
2016-07-28 17:58:12 +0700

    google

--- app/helpers/frontend/sessions_helper.rb
+++ app/helpers/frontend/sessions_helper.rb
@@ -6,16 +6,8 @@ module Frontend::SessionsHelper
 
 	# Returns a current user
 	def current_user
-		if (user_id = session[:user_id])
-			@current_user ||= User.find_by(id: user_id)
-		elsif (user_id = cookies.signed[:user_id])
-			user = User.find_by(id: user_id)
-			if user && user.authenticated?(:remember, cookies[:remember_token])
-				log_in_user(user)
-				@current_user = user
-			end
-		end
-	end
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
 	# User must login
 	def logged_in_user
